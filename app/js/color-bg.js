c2fd293d82e9da57f086da1271e17f2074daaa1f
dollarpo8 <radchyk9696@gmail.com>
2020-07-01 11:28:40 +0300

    'second'

--- app/js/color-bg.js
+++
@@ -1,5 +0,0 @@
-$('li').each(function() {
-    var back = ["green","blue","gray","yellow","orange","purple","black"];
-    var rand = back[Math.floor(Math.random() * back.length)];
-    $(this).css('background',rand);
-  });
